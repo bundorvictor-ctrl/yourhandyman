972622cd6e44cdadaf112a4af831771f8d458e20
Madu <bundorvictor@gmail.com>
2026-05-15 11:13:37 +0000

    Initial commit for Your Handy Man project

---
+++ handyman/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Skill, WorkerProfile
+
+admin.site.register(Skill)
+admin.site.register(WorkerProfile)
+
+# Register your models here.
---
+++ handyman/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class WorkerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill)
+    phone_number = models.CharField(max_length=20)
+    location = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
